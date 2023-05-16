a8844419f79f84233a98ef57cb99fc2004af3123
Mikhail Bashkirov <mikhail.bashkirov@ing.com>
2023-05-16 17:50:32 +0200

    feat: make compatible with @storybook/addon-interactions by fixing process

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
 import rollupAliasPlugin from '@rollup/plugin-alias';
 import rollupCommonjsPlugin from '@rollup/plugin-commonjs';
+import rollupInjectPlugin from '@rollup/plugin-inject';
 import rollupJsonPlugin from '@rollup/plugin-json';
+import rollupReplacePlugin from '@rollup/plugin-replace';
+import { stringifyProcessEnvs } from '@storybook/core-common';
 import { globals } from '@storybook/preview/globals';
 import type { Builder } from '@storybook/types';
 import { DevServerConfig, startDevServer } from '@web/dev-server';
 import type { DevServer } from '@web/dev-server-core';
 import { fromRollup } from '@web/dev-server-rollup';
+import detectFreePort from 'detect-port';
 import express from 'express';
 import { dirname, join } from 'path';
 import rollupExternalGlobalsPlugin from 'rollup-plugin-external-globals';
 import { koaToExpress } from './koa-to-express';
 import { storybookBuilderPlugin } from './storybook-builder-plugin';
-import detectFreePort from 'detect-port';
 
 const aliasPlugin = fromRollup(rollupAliasPlugin);
 const commonjsPlugin = fromRollup(rollupCommonjsPlugin);
 const externalGlobalsPlugin = fromRollup(rollupExternalGlobalsPlugin);
+const injectPlugin = fromRollup(rollupInjectPlugin);
 const jsonPlugin = fromRollup(rollupJsonPlugin);
+const replacePlugin = fromRollup(rollupReplacePlugin);
 
 // Storybook's Stats are optional Webpack related property
 type WdsStats = {
@@ -50,6 +55,7 @@ export const start: WdsBuilder['start'] = async ({
     express.static(previewDirOrigin, { immutable: true, maxAge: '5m' }),
   );
 
+  const env = await options.presets.apply<Record<string, string>>('env');
 
   // @ts-ignore detectFreePort works without number but type is incorrect
   const freePort = await detectFreePort();
@@ -71,14 +77,24 @@ export const start: WdsBuilder['start'] = async ({
           aliasPlugin({
             entries: {
               assert: 'browser-assert',
+              process: 'process/browser.js',
             },
           }),
           storybookBuilderPlugin(options),
+          replacePlugin({
+            // covers known "process.env.*" values and helps to remove dev code from production build
+            ...stringifyProcessEnvs(env),
+          }),
           externalGlobalsPlugin(globals),
           commonjsPlugin({
             requireReturnsDefault: 'preferred',
           }),
           jsonPlugin(),
+          injectPlugin({
+            preventAssignment: true,
+            // covers usages of "process" other than known "process.env.*" values
+            process: 'process',
+          }),
         ],
       },
     });
